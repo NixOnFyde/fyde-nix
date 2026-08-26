b851e2f406ade23a67996fe2434e725aa6a60971
skifli <121291719+skifli@users.noreply.github.com>
2026-08-26 15:01:35 +0100

    fix(tablet-mode): `pgrep` is not in `procps`

--- modules/fydetab-duo/input.nix
+++ modules/fydetab-duo/input.nix
@@ -18,7 +18,7 @@ in
     # Grant the active seat session (greeter + user) access to the himax
     # touch/stylus devices. TAG+="uaccess" relies on logind which is async
     # and can be slow. Users should also be in the "input" group for instant
-    # access via Unix group permissions (devices are root:input 0660).
+    # access through Unix group permissions (devices are root:input 0660).
     services.udev.extraRules = ''
       KERNEL=="event*", SUBSYSTEM=="input", ATTRS{name}=="himax-touchscreen", TAG+="uaccess"
       KERNEL=="event*", SUBSYSTEM=="input", ATTRS{name}=="himax-stylus", TAG+="uaccess"
