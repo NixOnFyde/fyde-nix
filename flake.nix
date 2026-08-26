057743c5d91adfcc027f739d180ced3f39df93c7
skifli <121291719+skifli@users.noreply.github.com>
2026-08-26 11:35:22 +0100

    fix(tablet-mode): use wvkbd from nixpkgs-unstable (has --auto)

--- flake.nix
+++ flake.nix
@@ -81,6 +81,7 @@
         system = "aarch64-linux";
         specialArgs = {
           inherit inputs;
+          nixpkgs-unstable = inputs.nixpkgs-unstable;
           fyde-nix = self;
         };
         modules = [
