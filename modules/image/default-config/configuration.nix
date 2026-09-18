34698b8e15793cd0287b580b591ff39ecb177cc1
skifli <121291719+skifli@users.noreply.github.com>
2026-09-18 11:32:41 +0100

    fix(default-config): reduce exemplar `scale`

--- modules/image/default-config/configuration.nix
+++ modules/image/default-config/configuration.nix
@@ -129,9 +129,9 @@
   hardware.fydetabduo.shell = {
     enable = true;
 
-    # Optional: scale the panel output (e.g., 1.5 for larger UI elements).
+    # Optional: scale the panel output (e.g., 1.25 for larger UI elements).
     # Applied using kanshi output scale in both orientations.
-    # desktop.scale = 1.5;
+    # desktop.scale = 1.25;
 
     power.autoProfile = {
       # Automatically change power profile based on current charge.
