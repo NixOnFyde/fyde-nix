75b696f0217295db499caf4ec7ceb83316a45fdb
skifli <121291719+skifli@users.noreply.github.com>
2026-09-18 12:01:36 +0100

    fix(bluetooth): remove erroneous `roles` hard-coding

--- modules/fydetab-duo/bluetooth.nix
+++ modules/fydetab-duo/bluetooth.nix
@@ -42,7 +42,6 @@ in
         "bluez5.enable-sbc-xq" = true;
         "bluez5.enable-msbc" = true;
         "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = [ "a2dp_sink" ]; # Prioritise high-quality playback
       };
     };
 
